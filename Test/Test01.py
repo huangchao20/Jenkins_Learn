4f6f739d012d857acee8466b9690db506af80cc6
huang <huangchao20@126.com>
2019-09-20 16:08:56 +0800

    【20190920】第五次提版

---
+++ Test/Test01.py
@@ -0,0 +1,9 @@
+def func01(n):
+	for i in range(1, n + 1):
+		for j in range(1, i + 1):
+			print("%s*%s=%s" % (j, i, i * j), end='\t')
+		print(end='\n')
+
+if __name__ == '__main__':
+    n = 110
+    func01(n)
